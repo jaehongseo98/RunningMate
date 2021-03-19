40d400d3c56082c7896623fc3e91526650b21c1b
공대표 <kongrpt@naver.com>
2021-03-19 15:02:40 +0900

    [ADD] Fragment2 Main페이지 지도 API 구현

--- app/src/main/java/com/example/project/Layout2Fragment.java
+++ app/src/main/java/com/example/project/Layout2Fragment.java
@@ -8,6 +8,9 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import net.daum.mf.map.api.MapView;
+
 // bottomNavigation 에서 매칭된 두번째 fragment
 public class Layout2Fragment extends Fragment {
 
@@ -15,6 +18,14 @@ public class Layout2Fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_layout2,container,false);
+
+        MapView mapView = new MapView(getActivity());
+        //this, getAcitity로 사용
+        ViewGroup mapViewContainer = (ViewGroup)root.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
         return root;
     }
+
+
 }
