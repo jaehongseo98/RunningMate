2bd7c88a784bd1e78772955cfefa77f9242f769e
공대표 <kongrpt@naver.com>
2021-03-29 16:52:50 +0900

    [수정] 메인 페이지 지도에 마커 표시

--- app/src/main/java/com/example/project/Layout2Fragment.java
+++ app/src/main/java/com/example/project/Layout2Fragment.java
@@ -2,6 +2,8 @@ package com.example.project;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -20,6 +22,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.skt.Tmap.TMapMarkerItem;
+import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 
 // bottomNavigation 에서 매칭된 두번째 fragment
@@ -34,6 +38,7 @@ public class Layout2Fragment extends Fragment {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_layout2, container, false);
         Button btnGps = (Button) root.findViewById(R.id.btn_Gps);
         TextView tvGps = (TextView) root.findViewById(R.id.tv_Gps);
+        TMapMarkerItem markerItem1 = new TMapMarkerItem(); //지도에 마커 설정
 
         LinearLayout linearLayoutTmap = (LinearLayout) root.findViewById(R.id.linearLayoutTmap);
         TMapView tMapView = new TMapView(root.getContext());
@@ -64,7 +69,16 @@ public class Layout2Fragment extends Fragment {
                             "경도 : " + latitude + "\n" +
                             "고도  : " + altitude);
 
-               
+                    tMapView.setCenterPoint(longitude, latitude);
+
+                    Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.border);
+                    TMapPoint tMapPoint1 = new TMapPoint(latitude,longitude);//내 위치 포인트 지정
+                    markerItem1.setTMapPoint(tMapPoint1); //마커에 Point 지정
+                    markerItem1.setVisible(TMapMarkerItem.VISIBLE); //마커의 VISIBLE 설정
+                    markerItem1.setIcon(bitmap); //마커의 이미지 설정
+                    tMapView.setCenterPoint(longitude, latitude); //맵의 중심이 마커로 이동
+                    tMapView.addMarkerItem("내 위치", markerItem1); //맵에 마커 표시
+
                 }
             }
         });
