6bb80fb22f8e65ff4c1ef7384da82d280ddc1804
공대표 <kongrpt@naver.com>
2021-04-02 16:37:58 +0900

    [ADD] 실시간 GPS 구현

--- app/src/main/java/com/example/project/Layout2Fragment.java
+++ app/src/main/java/com/example/project/Layout2Fragment.java
@@ -31,25 +31,33 @@ public class Layout2Fragment extends Fragment {
 
     Button btnGps;
     TextView tvGps;
+    LinearLayout linearLayoutTmap;
+    TMapView tMapView;
+    TMapMarkerItem markerItem1;
+    String provider;
+    double longitude;
+    double latitude;
+    double altitude;
+    LocationManager lm;
+    Bitmap bitmap;
+    TMapPoint tMapPoint1;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_layout2, container, false);
-        Button btnGps = (Button) root.findViewById(R.id.btn_Gps);
-        TextView tvGps = (TextView) root.findViewById(R.id.tv_Gps);
-        TMapMarkerItem markerItem1 = new TMapMarkerItem(); //지도에 마커 설정
-
-        LinearLayout linearLayoutTmap = (LinearLayout) root.findViewById(R.id.linearLayoutTmap);
-        TMapView tMapView = new TMapView(root.getContext());
+        btnGps = (Button) root.findViewById(R.id.btn_Gps);
+        tvGps = (TextView) root.findViewById(R.id.tv_Gps);
+        markerItem1 = new TMapMarkerItem(); //지도에 마커 설정
+        linearLayoutTmap = (LinearLayout) root.findViewById(R.id.linearLayoutTmap);
+        tMapView = new TMapView(root.getContext());
 
 
         tMapView.setSKTMapApiKey("l7xxbe621f3bb7e5409bbc8dd77c60a81ca3");
         linearLayoutTmap.addView(tMapView);
 
 
-        final LocationManager lm = (LocationManager) root.getContext().getSystemService(Context.LOCATION_SERVICE);
-
+        lm = (LocationManager) root.getContext().getSystemService(Context.LOCATION_SERVICE);
         btnGps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,10 +67,19 @@ public class Layout2Fragment extends Fragment {
                             0);
                 } else {
                     Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    String provider = location.getProvider();
-                    double longitude = location.getLongitude();
-                    double latitude = location.getLatitude();
-                    double altitude = location.getAltitude();
+
+                    if(location == null){
+                        provider = "초기";
+                        longitude = 0.0;
+                        latitude = 0.0;
+                        altitude = 0.0;
+                        //location 자체에 마지막 위치 정보가 저장이 안 돼 있으면 NPE 가 뜨니 초기에 선언을 해줘야 함.
+                    }else {
+                        provider = location.getProvider();
+                        longitude = location.getLongitude();
+                        latitude = location.getLatitude();
+                        altitude = location.getAltitude();
+                    }
 
                     tvGps.setText("위치정보 : " + provider + "\n" +
                             "위도 : " + longitude + "\n" +
@@ -71,15 +88,19 @@ public class Layout2Fragment extends Fragment {
 
                     tMapView.setCenterPoint(longitude, latitude);
 
-                    Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.border);
-                    TMapPoint tMapPoint1 = new TMapPoint(latitude,longitude);//내 위치 포인트 지정
+                    bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.border);
+                    tMapPoint1 = new TMapPoint(latitude, longitude);//내 위치 포인트 지정
                     markerItem1.setTMapPoint(tMapPoint1); //마커에 Point 지정
                     markerItem1.setVisible(TMapMarkerItem.VISIBLE); //마커의 VISIBLE 설정
                     markerItem1.setIcon(bitmap); //마커의 이미지 설정
                     tMapView.setCenterPoint(longitude, latitude); //맵의 중심이 마커로 이동
                     tMapView.addMarkerItem("내 위치", markerItem1); //맵에 마커 표시
 
+                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,1,gpsLocationListener);
+                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000,1,gpsLocationListener);
+
                 }
+
             }
         });
         return root;
@@ -88,10 +109,10 @@ public class Layout2Fragment extends Fragment {
     final LocationListener gpsLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
 
-            String provider = location.getProvider();
-            double longitude = location.getLongitude();
-            double latitude = location.getLatitude();
-            double altitude = location.getAltitude();
+            provider = location.getProvider();
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            altitude = location.getAltitude();
 
             tvGps.setText("위치정보 : " + provider + "\n" +
                     "위도 : " + longitude + "\n" +
