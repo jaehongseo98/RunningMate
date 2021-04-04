53a37692025cce0bf9e6c85301b34b8a9971af46
seojaehong <jaehong.seo82@gmail.com>
2021-04-04 19:31:16 +0900

    유저 업데이트 기본 email값 세팅

--- app/src/main/java/com/example/project/UserUpdateActivity.java
+++ app/src/main/java/com/example/project/UserUpdateActivity.java
@@ -1,10 +1,12 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
@@ -13,17 +15,28 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserUpdateActivity extends AppCompatActivity {
 
     Button updatepw, btnjuso, btnexit;
+    TextView useremail;
     private WebView webView;
     private TextView txt_address;
     private Handler handler;
     private WebSettings webSettings;
-    private FirebaseAuth auth;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +44,37 @@ public class UserUpdateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_update);
 
 
+        useremail = (TextView)findViewById(R.id.userid);
         txt_address = (TextView)findViewById(R.id.txt_address);
         updatepw = (Button)findViewById(R.id.updatepw);
         btnjuso = (Button)findViewById(R.id.btnjuso);
         btnexit = (Button)findViewById(R.id.btn_exit);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+
+        reference.child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    String key = snapshot1.getKey();
+                    if(user.getUid().equals(key)){
+                        UserInfo getemail = snapshot1.getValue(UserInfo.class);
+                        String email = getemail.email;
+                        Log.i("email",email);
+
+
+                        useremail.setText(email);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("TAG", "loadPost:onCancelled", error.toException());
+            }
+        });
 
 //        btnexit.setOnClickListener(new View.OnClickListener() {
 //            @Override
