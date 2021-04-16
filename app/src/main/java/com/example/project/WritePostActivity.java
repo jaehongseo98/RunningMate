ee540baa2349146afd48f8e81cb1a5fc118fde93
JungheeAn <wjdgml4744@gmail.com>
2021-04-16 15:58:03 +0900

    [MOD] 게시판 이미지 추가 수정

--- app/src/main/java/com/example/project/WritePostActivity.java
+++ app/src/main/java/com/example/project/WritePostActivity.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
@@ -15,17 +16,32 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 //게시글 파이어베이스에 저장
 public class WritePostActivity extends BasicActivity {
         private static final String TAG = "WritePostActivity";
         private FirebaseUser user;
+        private ArrayList<String> pathList = new ArrayList<>();
+        private LinearLayout parent;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +60,9 @@ public class WritePostActivity extends BasicActivity {
                 case 0: {
                     if (resultCode == Activity.RESULT_OK) {
                         String profilePath = data.getStringExtra("profilePath");
+                        pathList.add(profilePath);
 
-                        LinearLayout parent = findViewById(R.id.contentsLayout);
+                        parent = findViewById(R.id.contentsLayout);
                         ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
                         ImageView imageView = new ImageView(WritePostActivity.this);
@@ -67,7 +84,7 @@ public class WritePostActivity extends BasicActivity {
             public void onClick(View v) {
                 switch (v.getId()) {
                     case R.id.btnWrite:
-                        Update();
+                        storageUpload();
                         break;
                     case R.id.btnImg:
                         myStartActivity(GalleryActivity.class, "image");
@@ -79,20 +96,68 @@ public class WritePostActivity extends BasicActivity {
             }
         };
 
-        private void Update() {
+        private void storageUpload() {
             final String title = ((EditText) findViewById(R.id.edtTitle)).getText().toString();
             final String contents = ((EditText) findViewById(R.id.edtContents)).getText().toString();
 
             if (title.length() > 0 && contents.length() > 0) {
+                ArrayList<String> contentsList = new ArrayList<>();
+                int pathCount = 0;
                 user = FirebaseAuth.getInstance().getCurrentUser();
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                StorageReference storageRef = storage.getReference();
+
+                for(int i = 0; i <parent.getChildCount(); i++){
+                    View view = parent.getChildAt(i);
+                    if(view instanceof EditText){
+                        String text = ((EditText)view).getText().toString();
+                        if(text.length() > 0) {
+                            contentsList.add(text);
+                        }
+                    }else {
+                        contentsList.add(pathList.get(pathCount));
+                        final StorageReference mountainImagesRef = storageRef.child("user/"+user.getUid() + "/" + pathCount + ".jpg");
+                        try{
+                            InputStream stream = new FileInputStream(new File(pathList.get(pathCount)));
+                            StorageMetadata metadata = new StorageMetadata.Builder().setCustomMetadata("index",""+pathCount).build();
+                            UploadTask uploadTask = mountainImagesRef.putStream(stream,metadata);
+                            uploadTask.addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle unsuccessful uploads
+                                }
+                            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    int index = Integer.parseInt(taskSnapshot.getMetadata().getCustomMetadata("index"));
+                                    mountainImagesRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri uri) {
+                                            Log.e("로그", "uri:"+uri);
+                                        }
+                                    });
+
+                                    //contentsList.set(index.uri);
+                                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                                    // ...
+                                }
+                            });
+                        } catch (FileNotFoundException e) {
+                            Log.e("로그", "에러" + e.toString());
+                        }
+                        pathCount++;
+                    }
+                }
+
                 WriteInfo writeInfo = new WriteInfo(title, contents, user.getUid());
-                uploader(writeInfo);
+                storeUpload(writeInfo);
+
             } else {
                 startToast("내용을 입력해주세요.");
             }
         }
 
-        private void uploader(WriteInfo writeInfo) {
+        private void storeUpload(WriteInfo writeInfo) {
             FirebaseFirestore db = FirebaseFirestore.getInstance();
             db.collection("posts").add(writeInfo)
                     .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
