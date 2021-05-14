48fb532e5aedf7d077aa86bfff43f995c34f6066
JungheeAn <wjdgml4744@gmail.com>
2021-05-14 16:44:26 +0900

    [ADD] 게시판 추가 및 수정

--- app/src/main/java/com/example/project/WritePostActivity.java
+++ app/src/main/java/com/example/project/WritePostActivity.java
@@ -11,10 +11,15 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -24,7 +29,9 @@ import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import static com.example.project.Util.isImageFile;
 import static com.example.project.Util.isStorageUrl;
+import static com.example.project.Util.isVideoFile;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -40,6 +47,9 @@ public class WritePostActivity extends BasicActivity {
     private StorageReference storageRef;
     private ArrayList<String> pathList = new ArrayList<>();
     private LinearLayout parent;
+    private RelativeLayout buttonsBackgroundLayout;
+    private EditText contentsEditText;
+    private EditText titleEditText;
     private PostInfo postInfo;
     int pathCount, successCount;
 
@@ -47,17 +57,59 @@ public class WritePostActivity extends BasicActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_post);
+//        setToolbarTitle("게시글 작성");
 
         parent = findViewById(R.id.contentsLayout);
+        buttonsBackgroundLayout = findViewById(R.id.buttonsBackgroundLayout);
+        contentsEditText = findViewById(R.id.contentsEditText);
+        titleEditText = findViewById(R.id.titleEditText);
 
         findViewById(R.id.check).setOnClickListener(onClickListener);
         findViewById(R.id.image).setOnClickListener(onClickListener);
         findViewById(R.id.video).setOnClickListener(onClickListener);
+        findViewById(R.id.imageModify).setOnClickListener(onClickListener);
+        findViewById(R.id.videoModify).setOnClickListener(onClickListener);
+        findViewById(R.id.delete).setOnClickListener(onClickListener);
 
         FirebaseStorage storage = FirebaseStorage.getInstance();
         storageRef = storage.getReference();
 
         postInfo = (PostInfo) getIntent().getSerializableExtra("postInfo");
+        postInit();
+    }
+
+    private void postInit() {
+        if (postInfo != null) {
+            titleEditText.setText(postInfo.getTitle());
+            ArrayList<String> contentsList = postInfo.getContents();
+            for (int i = 0; i < contentsList.size(); i++) {
+                String contents = contentsList.get(i);
+                if (isStorageUrl(contents)) {
+                    pathList.add(contents);
+//                    ContentsItemView contentsItemView = new ContentsItemView(this);
+//                    parent.addView(contentsItemView);
+//
+//                    contentsItemView.setImage(contents);
+//                    contentsItemView.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            buttonsBackgroundLayout.setVisibility(View.VISIBLE);
+//                            selectedImageVIew = (ImageView) v;
+//                        }
+//                    });
+//
+//                    contentsItemView.setOnFocusChangeListener(onFocusChangeListener);
+//                    if (i < contentsList.size() - 1) {
+//                        String nextContents = contentsList.get(i + 1);
+//                        if (!isStorageUrl(nextContents)) {
+//                            contentsItemView.setText(nextContents);
+//                        }
+//                    }
+                } else if (i == 0) {
+                    contentsEditText.setText(contents);
+                }
+            }
+        }
     }
 
     @Override
@@ -93,7 +145,6 @@ public class WritePostActivity extends BasicActivity {
             switch (v.getId()) {
                 case R.id.check:
                     storageUpload();
-
                     break;
                 case R.id.image:
                     myStartActivity(GalleryActivity.class, "image");
@@ -132,6 +183,13 @@ public class WritePostActivity extends BasicActivity {
                             String path = pathList.get(pathCount);
                             successCount++;
                             contentsList.add(path);
+                            if(isImageFile(path)){
+                                formatList.add("image");
+                            }else if (isVideoFile(path)){
+                                formatList.add("video");
+                            }else{
+                                formatList.add("text");
+                            }
                             String[] pathArray = path.split("\\.");
                             final StorageReference mountainImagesRef = storageRef.child("post/" + documentReference.getId() + "/" + pathCount + "." + pathArray[pathArray.length - 1]);
                             try {
@@ -165,11 +223,18 @@ public class WritePostActivity extends BasicActivity {
             }
         }
 
-        private void storeUpload(DocumentReference documentReference, PostInfo PostInfo) {
-            documentReference.set(PostInfo)
-                    .addOnSuccessListener(aVoid -> Log.d(TAG, "DocumentSnapshot successfully writen!"))
+        private void storeUpload(DocumentReference documentReference, final PostInfo postInfo) {
+            documentReference.set(postInfo.getPostInfo())
+                    .addOnSuccessListener(aVoid -> {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                        Intent resultIntent = new Intent();
+                        resultIntent.putExtra("postinfo", postInfo);
+                        setResult(Activity.RESULT_OK, resultIntent);
+                        finish();
+                    })
                     .addOnFailureListener(e -> Log.w(TAG, "Error writing document", e));
         }
+
 
         private void startToast(String msg) {
             Toast.makeText(WritePostActivity.this, msg, Toast.LENGTH_SHORT).show();
